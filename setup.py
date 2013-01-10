39be87e4d213f86d7a2aef4f443907d01fcd7090
Hong Minhee <minhee@dahlia.kr>
2013-01-11 00:37:04 +0900

    Use with statement for reading readme file
    1. Without file closing it may cause "too many files" error
       on PyPy.  It's complicated to explain, but related to difference
       of memory handling from CPython (reference counting) to
       PyPy (garbage collection).
    
    2. README.md could be missed from distribution.  For this
       now it returns empty string when it faces to IOError.

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+from __future__ import with_statement
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+
+def get_readme():
+    try:
+        with open('README.md') as f:
+            return f.read().strip()
+    except IOError:
+        return ''
+
+
 setup(
     name='itunes-iap',
     version=__import__('itunesiap').__version__,
     description='Itunes In-app purchase validation api.',
-    long_description=open('README.md').read(),
+    long_description=get_readme(),
     author='Jeong YunWon',
     author_email='itunesiap@youknowone.org',
     url='https://github.com/youknowone/itunes-iap',
