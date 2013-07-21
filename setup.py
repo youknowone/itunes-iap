ad3e1c72ea8be5bce36039c46e63fa95f84e1b2d
Thomas Bartelmess <tbartelmess@marketcircle.com>
2013-07-21 08:04:48 -0400

    Added classifiers to setup.py
    Added classifier for Intended Audience, OS and Programming language

--- setup.py
+++ setup.py
@@ -39,4 +39,15 @@ setup(
     install_requires=[
         'distribute', 'requests', 'prettyexc>=0.4.1', 'six'
     ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+     ],
 )
