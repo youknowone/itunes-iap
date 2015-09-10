309acfa0297de174c6de8d8d37db5e914115c87f
Sergei Khoroshilov <khoroshilov@mybook.ru>
2015-09-10 19:00:26 +0300

    run tests with tox rather than setup.py test

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from __future__ import with_statement
-import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 
 def get_version():
@@ -18,28 +16,6 @@ def get_readme():
         return ''
 
 
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
-
-
 requirements = [
     'requests==2.7.0',
     'six==1.9.0',
@@ -47,12 +23,11 @@ requirements = [
 ]
 
 test_requirements = [
-    'tox==2.1.1',
-    'pytest==2.7.2',
-    'vcrpy==1.7.3',
-    'mock==1.3.0',
+    'tox>=2.1.1',
+    'pytest>=2.7.2',
+    'vcrpy>=1.7.3',
+    'mock>=1.3.0',
 ]
-
 
 setup(
     name='itunes-iap',
@@ -80,5 +55,7 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
     tests_require=requirements + test_requirements,
-    cmdclass={'test': Tox},
+    extras_require={
+        'test': test_requirements,
+    },
 )
