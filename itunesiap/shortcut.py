c460004a0ed3339f4c6b827ba5b3059a424cd555
Jeong YunWon <jeong@youknowone.org>
2015-11-18 09:57:19 +0900

    Support inline environment ops for shortcut

--- itunesiap/shortcut.py
+++ itunesiap/shortcut.py
@@ -2,5 +2,15 @@
 from .request import Request
 
 
-def verify(*args, **kwargs):
-    return Request(*args, **kwargs).verify()
+def verify(receipt_data, password=None, **kwargs):
+    """Shortcut API for :class:`itunesiap.request.Request`
+
+    :param str receipt_data: An iTunes receipt data as Base64 encoded string.
+    :param bool use_production: Override environment value if given
+    :param bool use_sandbox: Override environment value if given
+    :param bool verify_ssl: Override environment value if given
+
+    :return: :class:`itunesiap.receipt.Receipt` object if succeed.
+    :raises: Otherwise raise a request exception.
+    """
+    return Request(receipt_data, password).verify(**kwargs)
