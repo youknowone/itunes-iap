b4d33e9cf3f80e39d3f86422c33c2159e0ca15a1
Marina Melo <marinampires@gmail.com>
2016-02-10 11:06:35 -0200

    Included a proxy url to access iTunes's validation url

--- itunesiap/shortcut.py
+++ itunesiap/shortcut.py
@@ -2,10 +2,11 @@
 from .request import Request
 
 
-def verify(receipt_data, password=None, **kwargs):
+def verify(receipt_data, password=None, proxy_url=None, **kwargs):
     """Shortcut API for :class:`itunesiap.request.Request`
 
     :param str receipt_data: An iTunes receipt data as Base64 encoded string.
+    :param proxy_url: A proxy url to access the itunes validation url
     :param bool use_production: Override environment value if given
     :param bool use_sandbox: Override environment value if given
     :param bool verify_ssl: Override environment value if given
@@ -13,4 +14,4 @@ def verify(receipt_data, password=None, **kwargs):
     :return: :class:`itunesiap.receipt.Receipt` object if succeed.
     :raises: Otherwise raise a request exception.
     """
-    return Request(receipt_data, password).verify(**kwargs)
+    return Request(receipt_data, password, proxy_url).verify(**kwargs)
